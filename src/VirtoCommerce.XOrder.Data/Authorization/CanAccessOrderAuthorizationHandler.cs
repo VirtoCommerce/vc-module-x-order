6d8a4ac4909f051f8e7919f33b73903644fbe84c
Oleg Zhuk <zhukoo@gmail.com>
2024-10-31 16:49:37 +0200

    VCST-2048: Use ClaimsPrincipalExtensions GetUserId
    feat: Replace custom code with ClaimsPrincipalExtensions that resolves user id and name from IdentityOptions settings.

--- src/VirtoCommerce.XOrder.Data/Authorization/CanAccessOrderAuthorizationHandler.cs
+++ src/VirtoCommerce.XOrder.Data/Authorization/CanAccessOrderAuthorizationHandler.cs
@@ -6,6 +6,7 @@ using VirtoCommerce.CustomerModule.Core.Model;
 using VirtoCommerce.CustomerModule.Core.Services;
 using VirtoCommerce.OrdersModule.Core.Model;
 using VirtoCommerce.Platform.Core;
+using VirtoCommerce.Platform.Core.Security;
 using VirtoCommerce.Platform.Security.Authorization;
 using VirtoCommerce.XOrder.Core.Queries;
 
@@ -85,8 +86,7 @@ namespace VirtoCommerce.XOrder.Data.Authorization
 
         private static string GetUserId(AuthorizationHandlerContext context)
         {
-            //PT-5375 use ClaimTypes instead of "name"
-            return context.User.FindFirstValue("name");
+            return context.User.GetUserId();
         }
 
         private static string GetMemberId(AuthorizationHandlerContext context)
