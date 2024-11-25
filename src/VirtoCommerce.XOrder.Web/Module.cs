179dff3ea42753f5bae3b428db5b7726e6865ba3
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2024-11-26 00:32:30 +0200

    feat: add scoped schema

--- src/VirtoCommerce.XOrder.Web/Module.cs
+++ src/VirtoCommerce.XOrder.Web/Module.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using VirtoCommerce.Platform.Core.Modularity;
 using VirtoCommerce.Platform.Core.Settings;
 using VirtoCommerce.StoreModule.Core.Model;
+using VirtoCommerce.Xapi.Core.Extensions;
 using VirtoCommerce.Xapi.Core.Infrastructure;
+using VirtoCommerce.Xapi.Core.Models;
 using VirtoCommerce.XOrder.Core;
+using VirtoCommerce.XOrder.Data;
 using VirtoCommerce.XOrder.Data.Extensions;
 
 namespace VirtoCommerce.XOrder.Web;
@@ -24,6 +28,9 @@ public class Module : IModule, IHasConfiguration
     public void PostInitialize(IApplicationBuilder appBuilder)
     {
         var serviceProvider = appBuilder.ApplicationServices;
+
+        var playgroundOptions = appBuilder.ApplicationServices.GetService<IOptions<GraphQLPlaygroundOptions>>();
+        appBuilder.UseSchemaGraphQL<ScopedSchemaFactory<DataAssemblyMarker>>(playgroundOptions?.Value?.Enable ?? true, "order");
 
         // settings
         var settingsRegistrar = serviceProvider.GetRequiredService<ISettingsRegistrar>();
