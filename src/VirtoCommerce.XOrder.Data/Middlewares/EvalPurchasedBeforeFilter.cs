e1c5201e4422a245c1b4f7db326df04de94ba917
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2025-05-14 12:19:55 +0200

    VCST-3276: Aggregation fix for isPurchased filter (#27)

--- src/VirtoCommerce.XOrder.Data/Middlewares/EvalPurchasedBeforeFilter.cs
+++ src/VirtoCommerce.XOrder.Data/Middlewares/EvalPurchasedBeforeFilter.cs
@@ -25,9 +25,35 @@ public class EvalPurchasedBeforeFilter : IAsyncMiddleware<IndexSearchRequestBuil
                 // add new filter
                 var filterName = $"{PurchasedProductDocumentPrefix}_{parameter.StoreId}";
                 parameter.AddTerms(new[] { $"{filterName}:{parameter.UserId}" });
+
+                UpdateAggregations(parameter, andFilter, filterName);
             }
         }
 
         await next(parameter);
+    }
+
+    private static void UpdateAggregations(IndexSearchRequestBuilder parameter, AndFilter andFilter, string filterName)
+    {
+        foreach (var aggregation in parameter.Aggregations)
+        {
+            if (aggregation.Filter is not AndFilter aggregationFilter)
+            {
+                continue;
+            }
+
+            var purchasedBeforeAggregationFilter = aggregationFilter.ChildFilters.OfType<TermFilter>().FirstOrDefault(x => x.FieldName == "isPurchased");
+            if (purchasedBeforeAggregationFilter != null)
+            {
+                aggregationFilter.ChildFilters.Remove(purchasedBeforeAggregationFilter);
+
+                purchasedBeforeAggregationFilter = andFilter.ChildFilters.OfType<TermFilter>().FirstOrDefault(x => x.FieldName == filterName);
+                if (purchasedBeforeAggregationFilter != null)
+                {
+                    var clonedFitler = purchasedBeforeAggregationFilter.CloneTyped();
+                    aggregationFilter.ChildFilters.Add(clonedFitler);
+                }
+            }
+        }
     }
 }
