2816fe0770cfb145cf0315ea9e1b9b9ef52e5c8b
Alexey Shibanov <83034617+alexeyshibanov@users.noreply.github.com>
2026-07-10 17:31:42 +0300

    VCST-5431: expose SectionName on order configuration item GraphQL type (#44)

--- src/VirtoCommerce.XOrder.Core/Schemas/OrderConfigurationItemType.cs
+++ src/VirtoCommerce.XOrder.Core/Schemas/OrderConfigurationItemType.cs
@@ -20,6 +20,7 @@ public class OrderConfigurationItemType : ExtendableGraphType<ConfigurationItem>
     {
         Field(x => x.Id, nullable: false).Description("Configuration item ID");
         Field(x => x.SectionId, nullable: false).Description("Configuration item section ID");
+        Field(x => x.SectionName, nullable: true).Description("Configuration item section name");
         Field(x => x.Name, nullable: true).Description("Configuration item name");
         Field(x => x.ProductId, nullable: true).Description("Configuration item product ID");
         Field(x => x.Sku, nullable: true).Description("Configuration item SKU");
