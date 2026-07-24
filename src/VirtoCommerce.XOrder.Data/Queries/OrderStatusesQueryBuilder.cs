83264f51084731802939305183b8083023a7eab9
Alexey Shibanov <83034617+alexeyshibanov@users.noreply.github.com>
2026-07-24 13:15:44 +0300

    VCST-5468: Resolve order mediators per request

--- src/VirtoCommerce.XOrder.Data/Queries/OrderStatusesQueryBuilder.cs
+++ src/VirtoCommerce.XOrder.Data/Queries/OrderStatusesQueryBuilder.cs
@@ -1,3 +1,4 @@
+using System;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using VirtoCommerce.Xapi.Core.Queries;
@@ -7,8 +8,14 @@ namespace VirtoCommerce.XOrder.Data.Queries;
 
 public class OrderStatusesQueryBuilder : LocalizedSettingQueryBuilder<OrderStatusesQuery>
 {
+    public OrderStatusesQueryBuilder(IAuthorizationService authorizationService)
+        : base(authorizationService)
+    {
+    }
+
+    [Obsolete("Use the constructor without IMediator. The mediator is resolved from context.RequestServices per request.", DiagnosticId = "VC0015", UrlFormat = "https://docs.virtocommerce.org/products/products-virto3-versions")]
     public OrderStatusesQueryBuilder(IMediator mediator, IAuthorizationService authorizationService)
-        : base(mediator, authorizationService)
+        : this(authorizationService)
     {
     }
 
